d7eaa3903a776d1b5735234dd873b0ea767b462a
sirloso <los@carlossantos.xyz>
2021-05-11 23:08:23 -0400

    added more to skeleton

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sirloso/bcd_dropbox_importer
 
 go 1.16
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/karrick/godirwalk v1.16.1 // indirect
+	github.com/spf13/cobra v1.1.3
+)
--- transferer/transferer.go
+++ transferer/transferer.go
@@ -1,12 +1,63 @@
 package transferer
 
 import (
-	"fmt"
 	"path/filepath"
+
+	"github.com/karrick/godirwalk"
 )
 
+// map[users][orders][rolls][photos]
+type Files map[string]map[string]map[string]string
+
+// Transfer : entry point to transfer the files
 func Transfer(from string, to string, verbose bool) {
-	pathFrom, _ := filepath.Abs(from)
-	pathTo, _ := filepath.Abs(to)
-	fmt.Println(pathFrom, pathTo, verbose)
+	pathFrom, err := filepath.Abs(from)
+	handleErr(err)
+	pathTo, err := filepath.Abs(to)
+	handleErr(err)
+
+	var fromFolders Files
+	var toFolders Files
+
+	// process source
+	err = godirwalk.Walk(pathFrom, &godirwalk.Options{
+		Callback: func(osPathname string, de *godirwalk.Dirent) error {
+			appendToList(&fromFolders, osPathname)
+			return nil
+		},
+		Unsorted: true, // (optional) set true for faster yet non-deterministic enumeration (see godoc)
+	})
+	handleErr(err)
+
+	// process from
+	err = godirwalk.Walk(pathTo, &godirwalk.Options{
+		Callback: func(osPathname string, de *godirwalk.Dirent) error {
+			appendToList(&toFolders, osPathname)
+			return nil
+		},
+		Unsorted: true, // (optional) set true for faster yet non-deterministic enumeration (see godoc)
+	})
+	handleErr(err)
+
+	err = startTransfer(&fromFolders, &toFolders)
+	handleErr(err)
+}
+
+func appendToList(list *Files, item string) {
+
+}
+
+// MarkAsUsed: mark the file/folder/photo as processed
+func MarkAsUsed(path string) {
+	// rename file/folder/photo
+}
+
+func handleErr(err error) {
+	if err != nil {
+		panic("path error")
+	}
+}
+
+func startTransfer(fromFolders *Files, toFolder *Files) error {
+	return nil
 }
